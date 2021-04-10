deef501853e1846704fd1f33ef78c15d47c86828
FireBallDevelopment <82263407+Ivan-Novomiast@users.noreply.github.com>
2021-04-10 13:11:12 +0200

    First Release

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+from tkhtmlview import *
+
+
+def home():
+    root = tk.Tk()
+    root.title("bit.miner.py")
+    root.geometry("400x650")
+    
+    
+
+
+    root.mainloop()
+
+home()
